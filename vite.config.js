f69fe2e8fe8bd51e1797443400b81159c92cd146
yuling <gb2013429@gmail.com>
2024-02-08 01:03:42 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2024Vue+Vite-pre4/',
+  base: '/2024Vue-Vite-pre4/',
   plugins: [
     vue(),
   ],
