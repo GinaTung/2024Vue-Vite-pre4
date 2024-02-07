6a6b43613ffa392d8a88e2f40548a66d2fc53822
yuling <gb2013429@gmail.com>
2024-02-08 00:56:14 +0800

    setup

---
+++ src/views/AboutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h3>{{ data }}</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      data:{}
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_PATH);
+    console.log(import.meta.env.VITE_TEXT);
+    const url = import.meta.env.VITE_PATH
+    this.$http.get(url).then((response) => {
+        // console.log(response)
+        this.data = response.data.results[0];
+        // console.log(this.data);
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
